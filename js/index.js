2dee3a3facace03a874ed93bbbc41fbd098e37f1
Josh <aftershock840@gmail.com>
2024-12-16 19:43:47 -0500

    updated code in index.js file to experiment with errors

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+
+/*
+const firstName = "Josh";
+console.log(firstName);
+const lastName = "Owens";
+console.log(lastName);
+const age = 36;
+const hair = "brown";
+const height = 177.8;
+
+let currentCar = "Nissan";
+let workCar = "GMC";
+let family = "yes";
+let isReadyToGo;
+
+function run() {
+    console.log(currentCar);
+}
+*/
